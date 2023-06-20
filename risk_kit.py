31c818ec1bb3e5f62e22b9b8a4e2c08ac7e30aad
Ahmet Cihan <56366772+ahmet794@users.noreply.github.com>
2023-06-20 19:07:59 +0100

    Add files via upload

--- risk_kit.py
+++ risk_kit.py
@@ -29,3 +29,14 @@ def get_ffme_returns():
     rets = rets/100
     rets.index = pd.to_datetime(rets.index, format="%Y%m").to_period("M")
     return rets
+
+def get_hfi_returns():
+    """
+    Load and format the EDHEC Hedge Fund Index Returns
+    """
+
+    hfi = pd.read_csv("data/edhec-hedgefundindices.csv",
+                      header = 0, index_col=0, parse_dates=True)
+    hfi = hfi/100
+    hfi.index = hfi.index.to_period("M")
+    return hfi
