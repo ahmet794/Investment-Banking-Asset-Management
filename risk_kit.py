cb9d85ac2d93e10a64825e04731186360f360b48
Ahmet Cihan <56366772+ahmet794@users.noreply.github.com>
2023-06-20 22:52:38 +0100

    VaR, CVaR

--- risk_kit.py
+++ risk_kit.py
@@ -42,6 +42,13 @@ def get_hfi_returns():
     hfi.index = hfi.index.to_period("M")
     return hfi
 
+def semideviation(r):
+    """
+    Returns the semideviation aka negative semideviation of r
+    r must be a Series or a DataFrame
+    """
+    is_negative = r < 0
+    return r[is_negative].std(ddof=0)
 
 def skewness(r):
     """
@@ -78,4 +85,72 @@ def is_normal(r, level=0.01):
     statistic, p_val = scipy.stats.jarque_bera(r)
     return p_val > level
 
- 
+from scipy.stats import norm
+def var_gaussian(r, level = 5):
+    """
+    Retuns the Parametric VaR of a Series or DataFrame
+    """
+
+    # Compute the Z score assuming it was Gaussian
+
+    z = norm.ppf(level/100)
+    return -(r.mean() + z*r.std(ddof=0))
+
+def var_gaussian(r, level = 5, modified = False):
+    """
+    Retuns the Parametric  Gaussian VaR of a Series or DataFrame
+    If "modified" is True, then the modified VaR is returned,
+    using the Cornish-Fisher modificiation
+    """
+
+    # Compute the Z score assuming it was Gaussian
+
+    z = norm.ppf(level/100)
+
+    if modified:
+        # modify the Z score based on observed skewness and kurtosis
+        s = skewness(r)
+        k = kurtosis(r)
+        z = (z + 
+                (z**2 - 1)*s/6 +
+                (z**3 -3*z) *(k - 3)/24 -
+                (2*z**3 - 5*z)*(s**2)/36
+            )
+    return -(r.mean() + z*r.std(ddof=0))
+
+import numpy as np
+def var_historic(r, level = 5):
+    """
+    VaR Historic
+    """
+    if isinstance(r, pd.DataFrame):
+        return r.aggregate(var_historic, level = level)
+    
+    elif isinstance(r, pd.Series):
+        return -np.percentile(r, level)
+    else:
+        raise TypeError("Expected r to be Series or DataFrame")
+    
+def cvar_historic(r, level = 5):
+    """
+    Computes the Conditional VaR of Series or DataFrame
+    """
+
+    if isinstance(r, pd.Series):
+        is_beyond = r <= -var_historic(r, level = level)
+        return -r[is_beyond].mean()
+    elif isinstance(r, pd.DataFrame):
+        return r.aggregate(cvar_historic, level = level)
+    else: 
+        raise TypeError("Expected r to be a Sereis or DataFrame")
+    
+def semideviation3(r):
+    """
+    Returns the semideviation aka negative semideviation of r
+    r must be a Series or a DataFrame, else raises a TypeError
+    """
+    excess= r-r.mean()                                        # We demean the returns
+    excess_negative = excess[excess<0]                        # We take only the returns below the mean
+    excess_negative_square = excess_negative**2               # We square the demeaned returns below the mean
+    n_negative = (excess<0).sum()                             # number of returns under the mean
+    return (excess_negative_square.sum()/n_negative)**0.5     # semideviation
