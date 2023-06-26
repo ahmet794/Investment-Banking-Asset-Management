7ac1aee6e58013735ea8d5b3cdae32ae83d6dcd6
Ahmet Cihan <ahmedcihan@me.com>
2023-06-26 15:26:47 +0100

    Functionality of portfolio optimization

--- risk_kit.py
+++ risk_kit.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import scipy
+import scipy.optimize as minimize
 def drawdown(return_series: pd.Series):
     """
     Takes a times series of asset returns
@@ -206,7 +207,7 @@ def portfolio_vol(weights, covmat):
     """
     return (weights.T @ covmat @ weights)**0.5
 
-def plot_2aef(n_points, er, cov, style):
+def plot_2aef(n_points, er, cov, style=".-"):
     """
     Plots the 2-asset efficient frontier
     """
@@ -221,3 +222,28 @@ def plot_2aef(n_points, er, cov, style):
         "Volatility": vols
     })
     return ef.plot.line(x = "Volatility", y="Returns", style=style)
+
+def minimize_vol(target_return, er, cov):
+    """
+    target_ret -> W
+    """
+
+    n = er.shape[0]
+    init_guess = np.repeat(1/n, n)
+    bounds = ((0.0, 1.0), )*n
+    return_is_target = {
+        'type': 'eq',
+        'args': (er,),
+        'fun': lambda weights, er: target_return - portfolio_return(weights, er)
+    }
+    weights_sum_to_1 = {
+        'type': 'eq',
+        'fun': lambda weights: np.sum(weights) - 1
+    }
+    results = minimize(portfolio_vol, init_guess,
+                       args=(cov,), method="SLSQP",
+                       options={'disp': False},
+                       constraints = (return_is_target, weights_sum_to_1),
+                       bounds = bounds
+                       )
+    return results.x
