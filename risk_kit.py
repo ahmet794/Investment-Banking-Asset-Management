c50428ecf2e57c023b1a8a17763532c5471777e2
Ahmet Cihan <ahmedcihan@me.com>
2023-07-14 19:39:34 +0100

    Updated gbm modelling

--- risk_kit.py
+++ risk_kit.py
@@ -448,17 +448,26 @@ def summary_stats(r, riskfree_rate=0.03):
 
 
 
-def gbm(n_years=10, n_scenarios=1000, mu=0.07, sigma=0.15, steps_per_year=12, s_0=100.0):
+def gbm(n_years = 10, n_scenarios=1000, mu=0.07, sigma=0.15, steps_per_year=12, s_0=100.0, prices=True):
     """
-    Evolution of a Stock Price using a Geometric Brownian Motion Model
+    Evolution of Geometric Brownian Motion trajectories, such as for Stock Prices through Monte Carlo
+    :param n_years:  The number of years to generate data for
+    :param n_paths: The number of scenarios/trajectories
+    :param mu: Annualized Drift, e.g. Market Return
+    :param sigma: Annualized Volatility
+    :param steps_per_year: granularity of the simulation
+    :param s_0: initial value
+    :return: a numpy array of n_paths columns and n_years*steps_per_year rows
     """
+    # Derive per-step Model Parameters from User Specifications
     dt = 1/steps_per_year
-    n_steps = int(n_years*steps_per_year)
-    xi = np.random.normal(size=(n,n_steps, n_scenarios))
-    rets = mu*dt + sigma*np.sqrt(dt)*xi
-    rets = pd.DataFrame(rets)
-    # to prices
-    prices = s_0*(1+rets).cumprod()
-    return prices
+    n_steps = int(n_years*steps_per_year) + 1
+    # the standard way ...
+    # rets_plus_1 = np.random.normal(loc=mu*dt+1, scale=sigma*np.sqrt(dt), size=(n_steps, n_scenarios))
+    # without discretization error ...
+    rets_plus_1 = np.random.normal(loc=(1+mu)**dt, scale=(sigma*np.sqrt(dt)), size=(n_steps, n_scenarios))
+    rets_plus_1[0] = 1
+    ret_val = s_0*pd.DataFrame(rets_plus_1).cumprod() if prices else rets_plus_1-1
+    return ret_val
 
 
