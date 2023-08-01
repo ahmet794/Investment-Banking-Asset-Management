232e6a2425d9f664fed162fadd6aa60a86cb03e9
Ahmet Cihan <ahmedcihan@me.com>
2023-08-01 23:46:46 +0100

    Black Litterman functionality added

--- Advanced Portfolio Construction and Analysis with Python/risk_kit.py
+++ Advanced Portfolio Construction and Analysis with Python/risk_kit.py
@@ -737,3 +737,43 @@ def proportional_prior(sigma, tau, p):
     helit_omega = p.dot(tau*sigma).dot(p.T)
     # Make a diag matrix from the diag elements of Omega
     return pd.DataFrame(np.diag(np.diag(helit_omega.values)), index = p.index, columns = p.index), 
+
+from numpy.linalg import inv
+
+def bl(w_prior, sigma_prior, p, q,
+                omega=None,
+                delta=2.5, tau=.02):
+    """
+# Computes the posterior expected returns based on 
+# the original black litterman reference model
+#
+# W.prior must be an N x 1 vector of weights, a Series
+# Sigma.prior is an N x N covariance matrix, a DataFrame
+# P must be a K x N matrix linking Q and the Assets, a DataFrame
+# Q must be an K x 1 vector of views, a Series
+# Omega must be a K x K matrix a DataFrame, or None
+# if Omega is None, we assume it is
+#    proportional to variance of the prior
+# delta and tau are scalars
+    """
+    if omega is None:
+        omega = proportional_prior(sigma_prior, tau, p)
+    # Force w.prior and Q to be column vectors
+    # How many assets do we have?
+    N = w_prior.shape[0]
+    # And how many views?
+    K = q.shape[0]
+    # First, reverse-engineer the weights to get pi
+    pi = implied_returns(delta, sigma_prior,  w_prior)
+    # Adjust (scale) Sigma by the uncertainty scaling factor
+    sigma_prior_scaled = tau * sigma_prior  
+    # posterior estimate of the mean, use the "Master Formula"
+    # we use the versions that do not require
+    # Omega to be inverted (see previous section)
+    # this is easier to read if we use '@' for matrixmult instead of .dot()
+    #     mu_bl = pi + sigma_prior_scaled @ p.T @ inv(p @ sigma_prior_scaled @ p.T + omega) @ (q - p @ pi)
+    mu_bl = pi + sigma_prior_scaled.dot(p.T).dot(inv(p.dot(sigma_prior_scaled).dot(p.T) + omega).dot(q - p.dot(pi).values))
+    # posterior estimate of uncertainty of mu.bl
+#     sigma_bl = sigma_prior + sigma_prior_scaled - sigma_prior_scaled @ p.T @ inv(p @ sigma_prior_scaled @ p.T + omega) @ p @ sigma_prior_scaled
+    sigma_bl = sigma_prior + sigma_prior_scaled - sigma_prior_scaled.dot(p.T).dot(inv(p.dot(sigma_prior_scaled).dot(p.T) + omega)).dot(p).dot(sigma_prior_scaled)
+    return (mu_bl, sigma_bl)
